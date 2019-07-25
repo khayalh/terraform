a5c5a73de5799cba33a2aed028551cfbdfefa42e
khayalh <xayal444@gmail.com>
2019-07-25 16:59:14 +0400

    adding files to terraform repository

---
+++ connections.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "956299bb-8825-464d-a682-38b7c674bc7c"
+  client_id = "sariyev.rustam@outlook.com"
+  client_secret = "WSXqaz123@321@"
+  tenant_id = "3a2eec0c1-632c-4c09-b0ed-b587b6d61cb3"
+}
