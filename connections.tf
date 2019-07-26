7e4f34a382c6e70bb98d665fed002f4c266e432e
khayalh <xayal444@gmail.com>
2019-07-26 21:14:39 +0400

    adding config to connections.tf

--- connections.tf
+++ connections.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
-  subscription_id = "956299bb-8825-464d-a682-38b7c674bc7c"
-  client_id = "sariyev.rustam@outlook.com"
-  client_secret = "WSXqaz123@321@"
-  tenant_id = "3a2eec0c1-632c-4c09-b0ed-b587b6d61cb3"
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
 }
+variable subscription_id {}
+variable client_id {}
+variable client_secret {}
+variable tenant_id {}
