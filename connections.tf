7aa0e2bffcd9833962d83e7aadfb5c078355cbb1
khayalh <xayal444@gmail.com>
2019-07-30 18:17:59 +0400

    add vsphere provider

--- connections.tf
+++ connections.tf
@@ -4,6 +4,12 @@ provider "azurerm" {
   client_secret = "${var.client_secret}"
   tenant_id = "${var.tenant_id}"
 }
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+}
+
 variable subscription_id {}
 variable client_id {}
 variable client_secret {}
